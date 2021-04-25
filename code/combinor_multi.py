ed477736639a498c16c6afd082a6baf5bfe208ac
fyr91 <feng0049@gmail.com>
2021-04-25 19:32:58 +0800

    added new assets - hands

--- code/combinor_multi.py
+++ code/combinor_multi.py
@@ -2,7 +2,7 @@
 # @Author: User
 # @Date:   2021-04-16 11:05:13
 # @Last Modified by:   fyr91
-# @Last Modified time: 2021-04-23 12:28:36
+# @Last Modified time: 2021-04-25 19:15:31
 import cv2
 from PIL import Image
 import os
@@ -15,26 +15,28 @@ from config import *
 num_rare_items = 0
 existing_designs = []
 
-_,_,tops = next(os.walk(TOP_DIR))
-_,_,glasses = next(os.walk(GLASSES_DIR))
+_, _, tops = next(os.walk(TOP_DIR))
+_, _, glasses = next(os.walk(GLASSES_DIR))
+_, _, hands = next(os.walk(HAND_DIR))
 
 normal_tops, rare_tops = split_nr(tops)
 normal_glasses, rare_glasses = split_nr(glasses)
+normal_hands, rare_hands = split_nr(hands)
 
-rows = 15
-cols = 30
-unit_size = (120,120)
+rows = 20
+cols = 40
+unit_size = (120, 120)
 generated_imgs = []
 
-while len(generated_imgs) < rows*cols:
+while len(generated_imgs) < rows * cols:
 
     # init and create empty final render
     got_rare = False
-    render = Image.new('RGB', (600,600))
-
+    render = Image.new('RGB', (600, 600))
 
     # select body type & color
     body_type = random.choice(BODY_TYPES)
+    # body_type = 'pd_04'
     body_color_prob = random.random()
     if body_color_prob > 0 and body_color_prob <= 0.1:
         body_color = 'blue'
@@ -49,10 +51,11 @@ while len(generated_imgs) < rows*cols:
     else:
         body_color = 'black'
 
-
     # determine accessories
-    got_top = random.random()<TOP_PROB
-    got_glasses = random.random()<GLASSES_PROB
+    got_top = random.random() < TOP_PROB
+    got_glasses = random.random() < GLASSES_PROB
+    got_right_hand = random.random() < RIGHT_HAND_PROB
+    got_left_hand = random.random() < LEFT_HAND_PROB
 
     if got_top:
         if random.random() < RARE_PROB:
@@ -72,6 +75,26 @@ while len(generated_imgs) < rows*cols:
     else:
         glasses_file = ''
 
+    if got_right_hand:
+        if random.random() < RARE_PROB:
+            got_rare = True
+            right_hand_file = random.choice(rare_hands)
+        else:
+            right_hand_file = random.choice(normal_hands)
+    else:
+        right_hand_file = ''
+
+    if got_left_hand and not got_right_hand:
+        if random.random() < RARE_PROB:
+            got_rare = True
+            left_hand_file = random.choice(rare_hands)
+        else:
+            left_hand_file = random.choice(normal_hands)
+        # # only allow one bag & one phone on hand
+        # if ('bag' in right_hand_file and 'bag' in left_hand_file) or ('phone' in right_hand_file and 'phone' in left_hand_file):
+        #     left_hand_file = ''
+    else:
+        left_hand_file = ''
 
     # check design existance
     design = []
@@ -79,13 +102,14 @@ while len(generated_imgs) < rows*cols:
     design.append(body_color)
     design.append(top_file)
     design.append(glasses_file)
+    design.append(right_hand_file)
+    design.append(left_hand_file)
     design_str = ",".join(design)
     if (design_str in existing_designs):
         continue
     else:
         existing_designs.append(design_str)
         print(f'generated {len(existing_designs)}/{rows*cols} unique designs', end="\r", flush=True)
-
 
     # render
     if got_rare:
@@ -95,20 +119,30 @@ while len(generated_imgs) < rows*cols:
         bg_color = random.choice(BG_COLORS)
     bg = Image.new('RGB', (600, 600), bg_color)
     # bg = Image.new('RGB', (600, 600), (253, 249, 241))
-    render.paste(bg, (0,0))
+    render.paste(bg, (0, 0))
     body = Image.open(f'../assets/base/{body_type}/{body_color}.png')
-    render.paste(body, (0,0), body)
+    render.paste(body, (0, 0), body)
     if got_top:
         top_path = osp.join(TOP_DIR, top_file)
         accessory = Image.open(top_path)
         x, y = get_top_left_coord(render, accessory, OFFSET[body_type]["top"][0], OFFSET[body_type]["top"][1])
-        render.paste(accessory, (x,y), accessory)
+        render.paste(accessory, (x, y), accessory)
     if got_glasses:
         glasses_path = osp.join(GLASSES_DIR, glasses_file)
         accessory = Image.open(glasses_path)
-        x, y = get_top_left_coord(render, accessory, OFFSET[body_type]["glasses"][0],  OFFSET[body_type]["glasses"][1])
-        render.paste(accessory, (x,y), accessory)
-
+        x, y = get_top_left_coord(render, accessory, OFFSET[body_type]["glasses"][0], OFFSET[body_type]["glasses"][1])
+        render.paste(accessory, (x, y), accessory)
+    if got_right_hand and right_hand_file != '':
+        right_hand_path = osp.join(HAND_DIR, right_hand_file)
+        accessory = Image.open(right_hand_path)
+        x, y = get_top_left_coord(render, accessory, OFFSET[body_type]["right_hand"][0], OFFSET[body_type]["right_hand"][1])
+        render.paste(accessory, (x, y), accessory)
+    if got_left_hand and left_hand_file != '':
+        left_hand_path = osp.join(HAND_DIR, left_hand_file)
+        accessory = Image.open(left_hand_path)
+        accessory = accessory.transpose(Image.FLIP_LEFT_RIGHT)
+        x, y = get_top_left_coord(render, accessory, OFFSET[body_type]["left_hand"][0], OFFSET[body_type]["left_hand"][1])
+        render.paste(accessory, (x, y), accessory)
 
     # cvt to cv2 and resize
     cv2_img = np.array(render)
@@ -118,7 +152,7 @@ while len(generated_imgs) < rows*cols:
 
 
 # generate image grid
-img_tile = [generated_imgs[i*cols:(i+1)*cols] for i in range(rows)]
+img_tile = [generated_imgs[i * cols:(i + 1) * cols] for i in range(rows)]
 res = render_tile(img_tile)
 
 # cv2.imshow('overlay', res)
@@ -129,5 +163,5 @@ res = render_tile(img_tile)
 # cv2.destroyAllWindows()
 
 # write to file
-cv2.imwrite('../res/result.png', res)
+cv2.imwrite('../res/result_test.png', res)
 print(f"\n{num_rare_items} rare items created")
--- code/config.py
+++ code/config.py
@@ -2,24 +2,27 @@
 # @Author: fyr91
 # @Date:   2021-04-20 10:27:18
 # @Last Modified by:   fyr91
-# @Last Modified time: 2021-04-20 12:05:12
+# @Last Modified time: 2021-04-25 19:21:07
 
 # Directory config
-BASE_DIR = '../assets/base/' # dir for base
-TOP_DIR = '../assets/top/' # dir for hat
-HAIR_DIR = '../assets/hair/' # dir for hair
-GLASSES_DIR = '../assets/glasses/' # dir for glasses
+BASE_DIR = '../assets/base/'  # dir for base
+TOP_DIR = '../assets/top/'  # dir for hat
+HAIR_DIR = '../assets/hair/'  # dir for hair
+GLASSES_DIR = '../assets/glasses/'  # dir for glasses
+HAND_DIR = "../assets/hands/"
 
 # Probabililty
 TOP_PROB = 0.5
 HAIR_PROB = 0
 GLASSES_PROB = 0.2
-RARE_PROB = 0.03
+LEFT_HAND_PROB = 0.6
+RIGHT_HAND_PROB = 0.6
+RARE_PROB = 0.04
 
 # BG color palette
 BG_COLORS = [(155, 93, 229), (241, 91, 181), (0, 187, 249), (0, 245, 212)]
 RARE_BG = (254, 228, 64)
-BODY_TYPES = ['pd_01','pd_02','pd_03','pd_04']
+BODY_TYPES = ['pd_01', 'pd_02', 'pd_03', 'pd_04']
 # BG_COLOR = [(255,255,255)]
 # BG_COLOR = [(237, 220, 210), (255, 241, 230), (253, 226, 228), (250, 210, 225), (197, 222, 221),
 #             (219, 231, 228), (240, 239, 235), (214, 226, 233), (188, 212, 230), (153, 193, 222)]
@@ -29,22 +32,29 @@ OFFSET = {
         "hair": [-4, 176],
         "glasses": [-4, 224],
         "top": [-4, 80],
+        "right_hand": [-92, 320],
+        "left_hand": [84, 320]
     },
     "pd_02": {
         "hair": [-4, 138],
         "glasses": [-4, 192],
         "top": [-4, 32],
+        "right_hand": [-172, 288],
+        "left_hand": [164, 288]
     },
     "pd_03": {
         "hair": [-4, 272],
         "glasses": [-4, 320],
         "top": [-4, 176],
+        "right_hand": [-30, 368],
+        "left_hand": [20, 368]
     },
     "pd_04": {
         "hair": [-4, 82],
         "glasses": [-4, 144],
         "top": [-4, 0],
+        "right_hand": [-92, 256],
+        "left_hand": [84, 256]
     },
 
 }
-
