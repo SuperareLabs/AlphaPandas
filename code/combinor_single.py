bcd2ed84ee23420e430ea4bc8ca1aaa397cd8457
fyr91 <feng0049@gmail.com>
2021-12-23 00:29:17 +0800

    generated first batch for 1500 pandas

--- code/combinor_single.py
+++ code/combinor_single.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: User
 # @Date:   2021-04-21 14:26:54
-# @Last Modified by:   yirui
-# @Last Modified time: 2021-12-22 12:06:09
+# @Last Modified by:   fyr91
+# @Last Modified time: 2021-12-22 22:51:24
 import cv2
 from PIL import Image
 import os
@@ -12,37 +12,44 @@ import random
 from render_utils import *
 from config import *
 
+rares = [
+	['pd_01','gold,demon_horns_gold_rare.png','gls_stylish_green_rare.png','bitcoin_rare.png'],
+    ['pd_02','gold,flowerhat_rainbow_rare.png','gls_futuristic_purple_rare.png','windmill_colorful_rare.png'],
+    ['pd_03','gold,crown_special_rare.png','gls_stylish_yellow_rare.png','handbag_pink_rare.png'],
+    ['pd_04','gold,octopus_hat_red_rare.png','gls_butterfly_rare.png','skeleton_head_gray_rare.png']
+]
+
 
 body_type = 'pd_03'
 body_color = 'gold'
-head_file = 'rare_27.png'
-glasses_file = 'rare_gls_03.png'
-hand_file = 'rare-6.png'
+head_file = 'crown_special_rare.png'
+glasses_file = 'gls_stylish_yellow_rare.png'
+hand_file = 'handbag_pink_rare.png'
 
-output = 'temp.png'
+output = '#1330.png'
 
-head_path = osp.join(TOP_DIR, head_file)
+head_path = osp.join(HEADS_DIR, head_file)
 glasses_path = osp.join(GLASSES_DIR, glasses_file)
 hand_path = osp.join(HAND_DIR, hand_file)
 
 bg_file = RARE_BG_FILES[2]
 bg = Image.open(f'../assets/bg/{bg_file}')
 # bg = Image.new('RGB', (592, 592), bg_color)
-render = Image.new('RGB', (592,592))
-render.paste(bg, (0,0))
+render = Image.new('RGB', (592, 592))
+render.paste(bg, (0, 0))
 
 body = Image.open(f'../assets/base/{body_type}/{body_color}.png')
-render.paste(body, (0,0), body)
+render.paste(body, (0, 0), body)
 
 # top
 accessory = Image.open(head_path)
-x, y = get_top_left_coord(render, accessory, OFFSET[body_type]["top"][0], OFFSET[body_type]["top"][1])
-render.paste(accessory, (x,y), accessory)
+x, y = get_top_left_coord(render, accessory, OFFSET[body_type]["head"][0], OFFSET[body_type]["head"][1])
+render.paste(accessory, (x, y), accessory)
 
 # glasses
 accessory = Image.open(glasses_path)
-x, y = get_top_left_coord(render, accessory, OFFSET[body_type]["glasses"][0],  OFFSET[body_type]["glasses"][1])
-render.paste(accessory, (x,y), accessory)
+x, y = get_top_left_coord(render, accessory, OFFSET[body_type]["glasses"][0], OFFSET[body_type]["glasses"][1])
+render.paste(accessory, (x, y), accessory)
 
 # right hand
 accessory = Image.open(hand_path)
--- code/generate_overview.py
+++ code/generate_overview.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: yirui
 # @Date:   2021-12-22 12:06:38
-# @Last Modified by:   yirui
-# @Last Modified time: 2021-12-22 12:14:40
+# @Last Modified by:   fyr91
+# @Last Modified time: 2021-12-23 00:24:10
 import os
 import cv2
 from config import *
@@ -10,10 +10,14 @@ from render_utils import *
 
 root = "../res/individuals"
 generated_imgs = []
-for i in range(ROWS*COLS):
-    generated_imgs.append(cv2.resize(cv2.imread(os.path.join(root, f"#{i}.png")), OVERVIEW_UNIT_SIZE))
+for i in range(ROWS * COLS):
+    print(f"processing {i+1}", end="\r", flush=True)
+    file_path = os.path.join(root, f"#{i+1}.png")
+    # print(file_path)
+    img = cv2.imread(file_path)
+    # print(img)
+    generated_imgs.append(cv2.resize(img, OVERVIEW_UNIT_SIZE))
 
 img_tile = [generated_imgs[i * COLS:(i + 1) * COLS] for i in range(ROWS)]
 res = render_tile(img_tile)
 cv2.imwrite('../res/overview.png', res)
-
